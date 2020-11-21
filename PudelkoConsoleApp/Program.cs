346254cda0623f756495117649b10c6a0365c43d
Ohgodwhydidyoutakeit <w.lipowski97@gmail.com>
2020-11-21 19:08:14 +0100

    Finished project

--- PudelkoConsoleApp/Program.cs
+++ PudelkoConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PudelkoLibrary;
+using System;
+using System.Collections.Generic;
 
 namespace PudelkoConsoleApp
 {
@@ -6,7 +8,27 @@ namespace PudelkoConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Pudelko> pudelka = new List<Pudelko>();
+            // pamietaj o rozmiarach geniuszu 
+            pudelka.Add(new Pudelko(4200, 3123, null, UnitOfMeasure.milimeter));
+            pudelka.Add(new Pudelko(45, 21, null, UnitOfMeasure.milimeter));
+            pudelka.Add(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter));
+            pudelka.Add(
+             new Pudelko(321, 654, 123, UnitOfMeasure.milimeter).Kompresuj()
+         );
+           
+            pudelka.Add(new Pudelko(233.76, 152.44, 123.456, UnitOfMeasure.centimeter));
+     
+   
+         
+
+            pudelka.Sort();
+
+            pudelka.ForEach(
+                (pudelko) => {
+                    Console.WriteLine(pudelko.ToString());
+                }
+            );
         }
     }
 }
